e58bb7a18b47250edcaf6b62c84fa40ce26cc423
maxjschuh <maximilianschuh@gmx.de>
2024-04-10 23:47:38 +0200

    resized profile image on all screen sizes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
